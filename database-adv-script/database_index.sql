36b3597b56c5a42fd569606d045823335f592d72
Yonas Lemma <143127905+Yonas-Lemma@users.noreply.github.com>
2025-07-04 19:49:34 +0200

    Create  database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,14 @@
+-- Create indexes for User table
+CREATE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_user_role ON User(role);
+
+-- Create indexes for Booking table
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+CREATE INDEX idx_booking_start_date ON Booking(start_date);
+CREATE INDEX idx_booking_end_date ON Booking(end_date);
+
+-- Create indexes for Property table
+CREATE INDEX idx_property_host_id ON Property(host_id);
+CREATE INDEX idx_property_location ON Property(location);
+CREATE INDEX idx_property_name ON Property(name);
