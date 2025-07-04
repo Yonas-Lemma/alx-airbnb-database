e472b8cf7a6b276dd82cd2d01a3414200a2535f2
Yonas Lemma <143127905+Yonas-Lemma@users.noreply.github.com>
2025-07-04 19:51:33 +0200

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,11 @@
+-- Measure query performance before adding indexes
+
+-- Before Adding Indexes: Measure performance for Booking by user_id
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 'some_user_id';
+
+-- Before Adding Indexes: Measure performance for Property by location
+EXPLAIN ANALYZE SELECT * FROM Property WHERE location = 'some_location';
+
 -- Create indexes for User table
 CREATE INDEX idx_user_email ON User(email);
 CREATE INDEX idx_user_role ON User(role);
@@ -12,3 +20,11 @@ CREATE INDEX idx_booking_end_date ON Booking(end_date);
 CREATE INDEX idx_property_host_id ON Property(host_id);
 CREATE INDEX idx_property_location ON Property(location);
 CREATE INDEX idx_property_name ON Property(name);
+
+-- Measure query performance after adding indexes
+
+-- After Adding Indexes: Measure performance for Booking by user_id
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 'some_user_id';
+
+-- After Adding Indexes: Measure performance for Property by location
+EXPLAIN ANALYZE SELECT * FROM Property WHERE location = 'some_location';
