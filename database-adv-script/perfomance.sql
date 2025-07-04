8d12ac86779c7905c52c47ff965d96f3eeb8668e
Yonas Lemma <143127905+Yonas-Lemma@users.noreply.github.com>
2025-07-04 19:53:45 +0200

    Create  perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,24 @@
+-- Initial query to retrieve all bookings along with user details, property details, and payment details
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date,
+    pay.payment_method
+FROM 
+    Booking b
+JOIN 
+    User u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+LEFT JOIN 
+    Payment pay ON b.booking_id = pay.booking_id;
