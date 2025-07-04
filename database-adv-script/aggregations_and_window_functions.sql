6dbe492500a09519ec6ef48e7b6d5bac71a6032a
Yonas Lemma <143127905+Yonas-Lemma@users.noreply.github.com>
2025-07-04 19:39:56 +0200

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -13,12 +13,12 @@ GROUP BY
 ORDER BY 
     total_bookings DESC;
 
--- 2. Use a window function to rank properties based on the total number of bookings they have received
+-- 2. Use a window function to assign a row number to properties based on the total number of bookings they have received
 SELECT 
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM 
     Property p
 LEFT JOIN 
